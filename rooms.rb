ae344de0ac443839cc62de19d669a3136e92b54c
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-23 21:06:42 +0100

    Added functionality to add songs to the rooms.

--- rooms.rb
+++ rooms.rb
@@ -1,10 +1,11 @@
 class Room
 
-    attr_reader :name
+    attr_reader :name #:playlist
 
     def initialize(name)
         @name = name
         @room = []
+        # @playlist = []
     end
 
     def check_guest_into_room(guest)
@@ -19,4 +20,11 @@ class Room
         @room.clear()
     end
 
+    def add_song_to_room(song)
+        @room.push(song)
+    end
+
+    def number_of_songs_in_room()
+        return @room.length()
+    end
 end
--- spec/rooms_spec.rb
+++ spec/rooms_spec.rb
@@ -11,6 +11,7 @@ class RoomTest < MiniTest::Test
     def setup
         @room = Room.new("Kanto")
 
+
         @guest_1 = Guest.new("Lucy")
         @guest_2 = Guest.new("Clara")
         @guest_3 = Guest.new("Stu")
@@ -25,11 +26,15 @@ class RoomTest < MiniTest::Test
         assert_equal(1, @room.number_of_guests_in_room())
     end
 
-    def test_check_guest_out_of_room
+    def test_check_guest_out_of_room()
         @room.check_guest_into_room(@guest_2)
         @room.check_guest_out_of_room()
         assert_equal(0, @room.number_of_guests_in_room())
+    end
 
+    def test_add_song_to_room()
+        @room.add_song_to_room(@song_1)
+        assert_equal(1, @room.number_of_songs_in_room())
     end
 
 
